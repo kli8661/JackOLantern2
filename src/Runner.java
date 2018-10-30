bd2b86dc03d370a81002f2cff9a97a43f57a9a68
Kent <kli8661@bths.edu>
2018-10-30 09:25:52 -0400

    Finished

--- src/Runner.java
+++ src/Runner.java
@@ -8,8 +8,15 @@ public class Runner {
         faceFeatures[4][0] = " ";
         faceFeatures[0][7] = " ";
         faceFeatures[4][7] = " ";
+        jack.edit(" ",1,2);
+        jack.edit(" ",1,5);
+        jack.edit(" ",3,1);
+        jack.edit(" ",3,2);
+        jack.edit(" ",3,3);
+        jack.edit(" ",3,4);
+        jack.edit(" ",3,5);
+        jack.edit(" ",3,6);
         String result = jack.toString();
-        jack.edit("*","1","3");
         System.out.println(result);
 
     }
